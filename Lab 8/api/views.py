70e4be851f864ac7fee3588816515d7d2f2415b7
mubarakkkova <157195253+mubarakkkova@users.noreply.github.com>
2025-03-31 00:04:32 +0500

    /
    .

---
+++ Lab 8/api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Product, Category
+
+def product_list(request):
+    products = Product.objects.all()
+    data = {'products': list(products.values())}
+    return JsonResponse(data)
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    data = {'product': product.__dict__}
+    return JsonResponse(data)
+
+def category_list(request):
+    categories = Category.objects.all()
+    data = {'categories': list(categories.values())}
+    return JsonResponse(data)
+
+def category_detail(request, id):
+    category = get_object_or_404(Category, id=id)
+    data = {'category': category.__dict__}
+# Create your views here.
